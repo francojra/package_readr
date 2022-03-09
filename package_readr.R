76143460cd7091d0230ca9738a593c38d4d7f031
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-03-08 23:23:50 -0300

    Add files via upload

--- package_readr.R
+++ package_readr.R
@@ -41,3 +41,40 @@ imdb_csv <- read_csv(file = "imdb.csv")
 # caminho da pasta do diretório.
 
 imdb_csv <- read_csv(file = "C:/Users/jeann/Documents/pRaticando-Softawer-R/package_readr/imdb.csv")
+
+## A maioria das funções de leitura do {readr} possuem argumentos muito úteis para resolver 
+## problemas de importação:
+
+## col_names=: indica se a primeira linha da base contém ou não o nome das colunas. Também pode 
+## ser utilizado para (re)nomear colunas.
+
+## col_types=: caso alguma coluna seja importada com a classe errada (uma coluna de números 
+## foi importada como texto, por exemplo), você pode usar esse argumento para especificar 
+## a classe das colunas.
+
+## locale=: útil para tratar problema de encoding.
+
+## skip=: pula linhas no começo do arquivo antes de iniciar a importação. Útil para 
+## quando o arquivo a ser importado vem com metadados ou qualquer tipo de texto nas 
+## primeiras linhas, antes da base.
+
+## na=: indica quais strings deverão ser considaras NA na hora da importação.
+
+## Em alguns países, como o Brasil, as vírgulas são utilizadas para separar as
+## casas decimais dos números, inviabilizando o uso de arquivos .csv. 
+
+##  Para importar bases de arquivos separados por ponto-e-vírgula no R, 
+## basta usar a função read_csv2().
+
+imdb_csv2 <- read_csv2("imdb2.csv")
+
+## Arquivos .txt em geral podem ser lidos com a função read_delim(). Além do caminho 
+## até o arquivo, você também precisa indicar qual é o caractere utilizado para 
+## separar as colunas da base. Um arquivo separado por tabulação, por exemplo, pode 
+## ser lido utilizando a o código abaixo. O código \t é uma forma textual de 
+## representar a tecla TAB.
+
+imdb_txt <- read_delim("imdb.txt", delim = "\t")
+
+## Repare que a sintaxe é igual a da função read_csv(). Em geral, as funções 
+## de importação do {tidyverse} terão sintaxe e comportamento muito parecidos.
